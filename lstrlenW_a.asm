4626670494c12ab1af76d33986373e1175c9cc65
Jacenty Andruszkiewicz <120141219+Jacenty-And@users.noreply.github.com>
2023-06-11 19:17:37 +0200

    lstrlenW_a.asm updated

--- lstrlenW_a.asm
+++ lstrlenW_a.asm
@@ -3,22 +3,28 @@
 public _lstrlenW_a
 .code
 _lstrlenW_a PROC
-;int lstrlenW(wchar_t* lpString) {
-;	for (int i = 0; ; i++) if (lpString[i] == 0) return i;
+; int lstrlenW(wchar_t* lpString) {
+;	 for (int i = 0; ; i++) if (lpString[i] == 0) return i;
 push ebp
 mov ebp, esp
+push ebx
+push esi
+push edi
 
 mov eax, [ebp+8] ; wchar_t* lpString
 sub eax, 2
 mov ecx, 0
-ptl:
+next_char:
 inc ecx
 add eax, 2
 cmp word ptr [eax], 0
-jnz ptl
+jnz next_char
 dec ecx
 mov eax, ecx
 
+pop edi
+pop esi
+pop ebx
 pop ebp
 ret
 _lstrlenW_a ENDP
